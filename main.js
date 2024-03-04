32202df1efbf0f8bd6b63154797d8c5ec6ef9290
Nafeesmerchand <160695676+Nafeesmerchand@users.noreply.github.com>
2024-03-04 18:42:42 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+// data types
+//string
+var a = "hello";
+console.log(a);
+//number
+var b = 10;
+console.log(b);
+//boolean
+var c = true;
+console.log(c);
+//bigint
+var bigNumber = 2000000n;
+console.log(bigNumber);
+//symbol
+var e = Symbol();
+console.log(e);
+//any
+var f = 10;
+f = "aa";
+console.log(f);
+//never
+var g;
+//undefined
+var h = undefined;
+//null
+var i = null;
